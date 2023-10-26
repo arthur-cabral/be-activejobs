6aa8867f73b460e61617bcd0c9e0eee167adbb9b
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-26 00:12:09 -0300

    chore: Adding DependencyInjection

---
+++ Backend/CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Application.Mapping;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var defaultConnection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseMySql(
+                    defaultConnection,
+                    new MySqlServerVersion(new Version(8, 0, 33)),
+                    b => b.MigrationsAssembly("ActiveJobs")
+                );
+
+            });
+
+            services.AddScoped<IJobService, JobService>();
+
+            services.AddScoped<IJobRepository, JobRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
