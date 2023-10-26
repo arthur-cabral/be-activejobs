b8e441ba5b5fc788f8c301f2f1bc274aca99b573
arthur-cabral <arthurmartins10052002@gmail.com>
2023-10-26 00:11:09 -0300

    chore: Implementing mapping in DTOs

---
+++ Backend/Application/Mapping/DomainToDTOMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTO.Job;
+using Application.DTO.Pagination;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Job, JobDTO>().ReverseMap();
+            CreateMap<PaginationParameters, PaginationParametersDTO>().ReverseMap();
+        }
+    }
+}
