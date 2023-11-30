f745870c7d5f12eec4337000a7bc7be517d2e9a6
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-30 18:47:00 -0300

    chore: Creating messageResponseDTO

---
+++ Backend/Application/DTO/Response/MessageResponseDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.Response
+{
+    public class MessageResponseDTO
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public MessageResponseDTO(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public MessageResponseDTO(bool success)
+        {
+            Success = success;
+        }
+    }
+}
