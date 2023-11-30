f694d38f25e11f2a59eebd465426a58af299d463
arthur-cabral <arthurmartins10052002@gmail.com>
2023-11-30 18:47:12 -0300

    Adding migrations in project

---
+++ Backend/ActiveJobs/Migrations/20231107185805_AlterTableJobsAddColumnActive.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ActiveJobs.Migrations
+{
+    public partial class AlterTableJobsAddColumnActive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Jobs",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Jobs");
+        }
+    }
+}
